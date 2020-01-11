bb33a7d82920d225dab826a23fe7a7ea10e745c4
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-11 14:44:06 +0100

    removed fluff

--- components/ui/Alert.js
+++ components/ui/Alert.js
@@ -6,7 +6,6 @@ class AlertDismissibleExample extends Component {
         super(props);
         this.state = {
             show: true,
-            pop: false
         };
         this.displayHandler = this.displayHandler.bind(this);
     }
@@ -39,23 +38,4 @@ class AlertDismissibleExample extends Component {
     }
 }
 
-// const AlertDismissibleExample = () => {
-//     // function AlertDismissibleExample() {
-//     const [show, setShow] = useState(true);
-//     //
-//     //     if (show) {
-//     return (
-//         <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-//             <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-//             <p>
-//                 Change this and that and try again. Duis mollis, est non commodo
-//                 luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-//                 Cras mattis consectetur purus sit amet fermentum.
-//             </p>
-//         </Alert>
-//     );
-// // }
-// //     return <Button onClick={() => setShow(true)}>Show Alert</Button>;
-// // }
-// };
 export default AlertDismissibleExample;
